4984fc044e9d188a069aa7877549d8c2440328f2
Rahul Ghose <hansum.rahul@gmail.com>
2018-12-23 17:16:27 +0530

    initial commit

---
+++ build/precache-manifest.892ef0407d44a98267598dd797e0e27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b29a2893ba37e89821fd",
+    "url": "/static/js/main.b29a2893.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "b29a2893ba37e89821fd",
+    "url": "/static/css/main.1230a550.chunk.css"
+  },
+  {
+    "revision": "f0114a46d1fe3e72e882435588ea831d",
+    "url": "/index.html"
+  }
+];
